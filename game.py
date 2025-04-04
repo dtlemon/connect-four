3c66aaa45ad0bc2b1ce04d607e641669a3ce4878
Owen Mockabee <omockabee@anderson.edu>
2025-04-04 13:34:45 -0400

    Implement random agent function

--- game.py
+++ game.py
@@ -17,3 +17,17 @@ def get_legal_moves(board):
     """
 
     return [(x, y) for x, y in product(range(7), range(6)) if board[y][x] == " "]
+
+def random_agent(board):
+    """
+    Random Agent
+
+    Very basic agent that simply selects a random legal move
+    """
+
+    while True:
+        x = random.randint(0, 7)
+        y = random.randint(0, 6)
+
+        if board[y][x] == " ":
+            return (x, y)
