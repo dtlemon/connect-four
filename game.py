8a9f80f1d9af9e3dee2c081399d2b3d7766dd2b7
Owen Mockabee <omockabee@anderson.edu>
2025-04-04 14:08:46 -0400

    Add function that checks for wins

--- game.py
+++ game.py
@@ -31,3 +31,37 @@ def random_agent(board):
 
         if board[y][x] == " ":
             return (x, y)
+
+
+def get_winner(board):
+    """
+    Returns the winner for a board state (X, O or no winner)
+
+    >>> get_winner([['X','X','X'],['O','O','X'],['X','O','O']])
+    'X'
+
+    """
+
+    # Check for Horizontal wins
+    for row in range(6):
+        for col in range(7 - 3):
+            if board[row][col] == board[row][col + 1] == board[row][col + 2] == board[row][col + 3] != " ":
+                return board[row][col]
+            
+    # Check for Vertical wins
+    for row in range(6 - 3):
+        for col in range(7):
+            if board[row][col] == board[row + 1][col] == board[row + 2][col] == board[row + 3][col] != " ":
+                return board[row][col]
+            
+    # Check for Diagonal wins that go bottom left to top right
+    for row in range(3, 6):
+        for col in range(7 - 3):
+            if board[row][col] == board[row - 1][col + 1] == board[row - 2][col + 2] == board[row - 3][col + 3] != " ":
+                return board[row][col]
+
+    # Check for diagonal wins that go bottom right to top left
+    for row in range(6 - 3):
+        for col in range(7 - 3):
+            if board[row][col] == board[row + 1][col + 1] == board[row + 2][col + 2] == board[row + 3][col + 3] != " ":
+                return board[row][col]
